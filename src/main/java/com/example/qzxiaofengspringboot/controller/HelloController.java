9bacc3baea706844aa3dbf2b0a6483badf39a90b
全栈xiaofeng <2746093347@qq.com>
2022-02-28 12:46:08 +0800

    继续完善

--- src/main/java/com/example/qzxiaofengspringboot/controller/HelloController.java
+++ src/main/java/com/example/qzxiaofengspringboot/controller/HelloController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/test")
+    @RequestMapping("/index")
     public String Index(){
         return "index";
     }
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
 }
+
